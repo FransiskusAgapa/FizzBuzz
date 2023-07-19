b6819219c6c43ea1be9e95e01a8ea366b7f8de16
Fransiskus Agapa <137876704+FransiskusAgapa@users.noreply.github.com>
2023-07-19 13:53:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const endNum = 100;
+const startNum = 1;
+
+for (let i = startNum; i < endNum; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
